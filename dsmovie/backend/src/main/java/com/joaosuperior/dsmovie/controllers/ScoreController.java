2f474614c9475de34150779c615a226f0f375604
JoaoMelo <joaomelo@inforgeneses.com.br>
2022-03-10 11:21:30 -0300

    Ajuste para o heroku

---
+++ dsmovie/backend/src/main/java/com/joaosuperior/dsmovie/controllers/ScoreController.java
@@ -0,0 +1,25 @@
+package com.joaosuperior.dsmovie.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joaosuperior.dsmovie.dto.MovieDTO;
+import com.joaosuperior.dsmovie.dto.ScoreDTO;
+import com.joaosuperior.dsmovie.services.ScoreService;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+	
+	@Autowired
+	private ScoreService service;
+	
+	@PutMapping
+	public MovieDTO SaveScore(@RequestBody ScoreDTO dto) {
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
+	}
+}
