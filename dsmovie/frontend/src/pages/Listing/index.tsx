7a1bb7ce852a47bdf91404196948f15c05dcbcb8
JoaoMelo <joaomelo@inforgeneses.com.br>
2022-03-11 13:53:37 -0300

    useState, useEffect

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -2,16 +2,28 @@ import MovieCard from "components/MovieCard";
 import axios from "axios";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "Utils/requests";
+import { useState, useEffect } from "react";
+import { MoviePage } from "types/movies";
 
 function Listing() {
 
-  // Teste forma errada
-  axios.get(`${BASE_URL}/movies?size=0`)
+  const [pageNumber, setPageNumber ] = useState(0);
+
+  useEffect(()=>{
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
     .then(response =>{
-      console.log(response.data);
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumber(data.number);
     });
+
+  }, []);
+
+  // Teste forma errada
+ 
   return (
     <>
+    <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
