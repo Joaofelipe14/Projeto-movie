66c156068f23a15fa5d35c888538f84489c8b20c
JoaoMelo <joaomelo@inforgeneses.com.br>
2022-03-11 14:18:44 -0300

    Props

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -6,39 +6,40 @@ import { useState, useEffect } from "react";
 import { MoviePage } from "types/movies";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
 
-  const [pageNumber, setPageNumber ] = useState(0);
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+        setPageNumber(data.number);
+      });
+  }, [pageNumber]);
 
-  useEffect(()=>{
-    axios.get(`${BASE_URL}/movies?size=12&page=1`)
-    .then(response =>{
-      const data = response.data as MoviePage;
-      console.log(data);
-      setPageNumber(data.number);
-    });
-
-  }, []);
-
-  // Teste forma errada
- 
   return (
     <>
-    <p>{pageNumber}</p>
-      <Pagination />
+      <p>{pageNumber}</p>
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div> <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div> <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div> <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div> <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {page.content.map((movie) => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
       </div>
     </>
