c64836fcea600c723ff3418daddc88610f34d806
JoaoMelo <joaomelo@inforgeneses.com.br>
2022-03-11 23:41:37 -0300

    Pagination

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -19,6 +19,11 @@ function Listing() {
     empty: true,
   });
 
+  const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+
+  }
+
   useEffect(() => {
     axios
       .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
@@ -31,7 +36,7 @@ function Listing() {
 
   return (
     <>
-    <Pagination />
+    <Pagination page={page} onChange={handlePageChange} />
 
       <div className="container">
         <div className="row">
